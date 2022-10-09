3711451d9d7ec66b2f58e2798636f72f274ffa45
PunyaSai182008 <104514490+PunyaSai182008@users.noreply.github.com>
2022-10-09 17:16:09 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,98 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+
+    {
+     numb: 6,
+     question: "What is Python?",
+     answer: "Python is an interpreted, object-oriented, high-level programming language with dynamic semantics",
+     options: [
+       "Python is an interpreted, object-oriented, high-level programming language with dynamic semantics",
+       "Python is very dangerous we can hack anyone using python",
+       "Python is a venomous snake in a computer system",
+       "Pyhton is a Colorful Style Sheet and an eXTra Multi-Program Language ",
+     ]
+   },
+
+   {
+    numb: 7,
+    question: "What is Java?",
+    answer: "It is designed to have as few implementation dependencies as possible",
+    options: [
+      "It is used in Minecraft so minecraft made java",
+      "It is nothing but made up of Javascript and it is scripted",
+      "It is designed to have as few implementation dependencies as possible",
+      "Java is an Structured Query Language made up of Hypertext processor",
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Do you like this QUIZ?",
+    answer: "I don't have time to answer🙄bye👋",
+    options: [
+      "No🤮",
+      "Yes very much❤😻",
+      "Yes ",
+      "I don't have time to answer🙄bye👋",
+    ]
+  },
+  
+
+
+   
+];
